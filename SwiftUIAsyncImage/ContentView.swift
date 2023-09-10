9e8c2b05da083f14d13be780d61827266d6f4448
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-10 14:47:00 -0300

    Extensions

--- SwiftUIAsyncImage/ContentView.swift
+++ SwiftUIAsyncImage/ContentView.swift
@@ -7,6 +7,22 @@
 
 import SwiftUI
 
+extension Image {
+    func imageModifier() -> some View {
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View {
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+}
+
 struct ContentView: View {
     private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
     
@@ -22,16 +38,9 @@ struct ContentView: View {
         // MARK: - 3. PLACEHOLDER
         
         AsyncImage(url: URL(string: imageURL)) { image in
-            image
-                .resizable()
-                .scaledToFit()
+            image.imageModifier()
         } placeholder: {
-            Image(systemName: "photo.circle.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(maxWidth: 128)
-                .foregroundColor(.purple)
-                .opacity(0.5)
+            Image(systemName: "photo.circle.fill").iconModifier()
         }
         .padding(40)
     }
