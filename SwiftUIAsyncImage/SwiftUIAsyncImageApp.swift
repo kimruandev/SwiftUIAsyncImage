a66485fddb60ebfa83a6df02b09a7b4a5caaf863
Kim Ruan Lopes <kim.ruan@prax.net.br>
2023-09-10 13:20:51 -0300

    Hello World!

---
+++ SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Kim Ruan on 10/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
